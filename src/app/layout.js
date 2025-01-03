b85800fdbbd956c15eb47acc4ece8a008968263d
Milan Pjevic <milanpjevic@icloud.com>
2025-01-03 17:20:24 +0100

    style: Apply Public Sans font and SCSS global styles

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,10 +2,20 @@
 
 import "@/styles/main.scss";
 
+import { Public_Sans } from "next/font/google";
+
+const publicSans = Public_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  style: "normal",
+  weight: "400",
+  variants: ["400", "700"],
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={publicSans.className}>{children}</body>
     </html>
   );
 }
