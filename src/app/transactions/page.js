ee8193dd1214d197d8c1ae61cd1281fd8cc95dfd
Milan Pjevic <milanpjevic@icloud.com>
2025-01-15 16:18:00 +0100

    Transactions: Table

--- src/app/transactions/page.js
+++ src/app/transactions/page.js
@@ -1,7 +1,29 @@
 /** @format */
 
+import Table from "@/components/Table/Table";
+import style from "./page.module.scss";
+import data from "../../data/data.json";
+const { transactions } = data;
+
 function Page() {
-  return <div>Transactions</div>;
+  return (
+    <div className={style.transactions}>
+      <h1 className={style.transactions__heading}>Transactions</h1>
+
+      <Table data={transactions} className={style.table}>
+        <Table.Header
+          columns={[
+            "Recipient / Sender",
+            "Catergory",
+            "Transaction Date",
+            "Amount",
+          ]}
+          className={style.table__header}
+        />
+        <Table.Body rows={transactions} />
+      </Table>
+    </div>
+  );
 }
 
 export default Page;
